32809c8877479445ad9f778d0fe202c1b14f8767
wmhxx <wangmenghe@sumervart.com>
2023-05-22 13:35:28 +0800

    [add] - 首次提交

---
+++ src/main/java/com/github/wmhxx/wechatclientbot/WeChatClientBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.wmhxx.wechatclientbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeChatClientBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatClientBotApplication.class, args);
+    }
+
+}
